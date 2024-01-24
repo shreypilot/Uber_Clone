996a6f3b140c657c0c88fbacdeb1de27e9303e7d
shreypilot <shreypilot28@gmail.com>
2024-01-25 00:53:20 +0530

    basic setup of uberClone

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Hello UberClone</Text>
     </View>
-  );
+  )
 }
 
+export default App
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+  container:{
+    flex:1
+  }
+  
+  
+  })
---
+++ Readme.md
@@ -0,0 +1,7 @@
+src->
+-components
+-screens -> where put all our screens
+-navigations 
+->contexts -> you're gonna have to manage some data at the global level 
+-reducers
+-globals->for global data for example global styles and dummy data  
