5e024ab8c7da69cb46909588a1e21121f5663dfe
shreypilot <shreypilot28@gmail.com>
2024-01-25 14:37:43 +0530

    setup locations

--- src/global/data.js
+++ src/global/data.js
@@ -7,8 +7,8 @@ export const filterData = [ {name:"Ride",image: require('../../assets/ride.png')
 
 
 export const rideData =[
-    {street:"32 Olivia Rd",area:"Klipfontein 83-Ir,Boksburg",id:"0"},
-    {street:"Hughes Industrial Park",area:"Hughes,Boksburg",id:"1"},
+    {street:"Chandi,Bihar,India",area:"Nalanda College of Engineering ",id:"0"},
+    {street:"Gandhi maidan",area:"Patna,bihar,india",id:"1"},
     {street:"32 Olivia Road",area:" East Rand,Ekurhuleni,Gauteng,1459",id:"2"},
     {street:"Total Boksburg",area:"35 Atlas Rd,Anderbolt,Boksburg",id:"3"},
     {street:"179 8th Ave",area:"Bezuidenhout Valley,Johannesburg",id:"4"},
@@ -57,9 +57,9 @@ export const paymentOptions = [{image:require('../../assets/visaIcon.png'),text:
 
 export const availableServices = ["Uber Go","UberX","Uber connect","Uber Black","Uber Van","Uber Assist"]
 
-export const carsAround = [{latitude:-26.207487,longitude:28.236226},
-    {latitude:-26.202616,longitude:28.227718},
-    {latitude:-26.202424,longitude:28.236612},
-    {latitude:-26.208565,longitude:28.237191},
-    {latitude:-26.203598,longitude:28.239509},
+export const carsAround = [{latitude:25.324530,longitude:85.404420},
+    {latitude:25.325580,longitude:85.403420},
+    {latitude:25.329590,longitude:85.404520},
+    {latitude:25.322030,longitude:85.404460},
+    {latitude:25.326510,longitude:85.404720}
 ]
